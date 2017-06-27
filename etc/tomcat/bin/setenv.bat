8ba6782ad8e487fa9893be9630c99a03d073003c
Drew Wills <wills.drew@gmail.com>
2017-06-26 19:07:43 -0700

    Use 'rem' to comment a line in a .bat file, not '#'

--- etc/tomcat/bin/setenv.bat
+++ etc/tomcat/bin/setenv.bat
@@ -1,10 +1,10 @@
-# Display settings at startup
+rem Display settings at startup
 set CATALINA_OPTS=%CATALINA_OPTS% -XX:+PrintCommandLineFlags
 
-# Discourage address map swapping by setting Xms and Xmx to the same value
-# http://confluence.atlassian.com/display/DOC/Garbage+Collector+Performance+Issues
+rem Discourage address map swapping by setting Xms and Xmx to the same value
+rem http://confluence.atlassian.com/display/DOC/Garbage+Collector+Performance+Issues
 set CATALINA_OPTS=%CATALINA_OPTS% -Xms64m
 set CATALINA_OPTS=%CATALINA_OPTS% -Xmx512m
 
-# Prevent "Unrecognized Name" SSL warning
+rem Prevent "Unrecognized Name" SSL warning
 set CATALINA_OPTS=%CATALINA_OPTS% -Djsse.enableSNIExtension=false
