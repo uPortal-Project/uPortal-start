9dd81bd8188444b4dab40abc76de69b1ebc576d4
Drew Wills <wills.drew@gmail.com>
2017-07-25 11:10:23 -0700

    UP-4910:  Introduce the 'portal.home' system property in the sample setenv.sh and setenv.bat files for Tomcat;  in uPortal, we can use this value to find the location of (optional) configuration files we want to source into the Spring Environment

--- etc/tomcat/bin/setenv.sh
+++ etc/tomcat/bin/setenv.sh
@@ -8,3 +8,9 @@ CATALINA_OPTS="$CATALINA_OPTS -Xmx512m"
 
 # Prevent "Unrecognized Name" SSL warning
 CATALINA_OPTS="$CATALINA_OPTS -Djsse.enableSNIExtension=false"
+
+# We need to send a 'portal.home' system property to the JVM;  use the value of PORTAL_HOME, if
+# present, or fall back to a value calculated from $CATALINA_BASE
+[ -z "$PORTAL_HOME" ] && PORTAL_HOME="$CATALINA_BASE/portal"
+echo "PORTAL_HOME=$PORTAL_HOME"
+CATALINA_OPTS="$CATALINA_OPTS -Dportal.home=$PORTAL_HOME"
