a726e47867e736bc57a021ea8de4c6e743fd541b
Brandon Powell <bpowell29a@gmail.com>
2017-10-24 10:36:36 -0400

    Sets G1GC to default if no garbage collector defined

--- etc/tomcat/bin/setenv.sh
+++ etc/tomcat/bin/setenv.sh
@@ -14,3 +14,13 @@ CATALINA_OPTS="$CATALINA_OPTS -Djsse.enableSNIExtension=false"
 [ -z "$PORTAL_HOME" ] && PORTAL_HOME="$CATALINA_BASE/portal"
 echo "PORTAL_HOME=$PORTAL_HOME"
 CATALINA_OPTS="$CATALINA_OPTS -Dportal.home=$PORTAL_HOME"
+
+# Checking if anyother garbage collectors have been defined. If no other garbage
+# collector is present, default to G1GC
+# List of options taken from:
+# http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/gc01/index.html
+echo $CATALINA_OPTS | grep -e '-XX:+UseSerialGC' -e '-XX:+UseParallelGC' -e '-XX:ParallelGCThreads' -e '-XX:+UseParallelOldGC' -e '-XX:+UseConcMarkSweepGC' -e '-XX:+UseParNewGC' -e '-XX:+CMSParallelRemarkEnabled' -e '-XX:CMSInitiatingOccupancyFraction' -e '-XX:+UseCMSInitiatingOccupancyOnly' -e '-XX:+UseG1GC'
+if [ $? -eq 1 ]
+then
+    CATALINA_OPTS="$CATALINA_OPTS -XX:+UseG1GC"
+fi
