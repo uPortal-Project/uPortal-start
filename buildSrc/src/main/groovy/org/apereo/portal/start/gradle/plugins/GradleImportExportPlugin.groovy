44e00d252b5cfda64bcf7b1ba29558bbfba7c734
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2019-02-26 16:24:17 +0100

    fix: dataImport task should not run tomcatClean and tomcatDeploy tasks
    This permit to avoid to undeploy and deploy apps when only a dataImport is run
    Should solve issue #252

--- buildSrc/src/main/groovy/org/apereo/portal/start/gradle/plugins/GradleImportExportPlugin.groovy
+++ buildSrc/src/main/groovy/org/apereo/portal/start/gradle/plugins/GradleImportExportPlugin.groovy
@@ -17,7 +17,8 @@ class GradleImportExportPlugin implements Plugin<Project> {
         }
         project.task('dataImport') {
             group 'Data'
-            dependsOn project.rootProject.tasks.portalProperties, project.tasks.tomcatDeploy
+            dependsOn project.rootProject.tasks.portalProperties
+            mustRunAfter project.tasks.tomcatDeploy
             doFirst {
                 if (project.tasks.dataImport.actions.size() == 1) {
                     logger.lifecycle('No actions have been defined for this task in this project')
