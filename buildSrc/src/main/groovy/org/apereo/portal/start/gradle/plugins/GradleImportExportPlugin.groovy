69f32325edc7a1a1e1f8f88b54bedf8671725370
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2019-04-05 18:34:06 +0200

    fix: launching task dataDelete should not run tomcatDeploy

--- buildSrc/src/main/groovy/org/apereo/portal/start/gradle/plugins/GradleImportExportPlugin.groovy
+++ buildSrc/src/main/groovy/org/apereo/portal/start/gradle/plugins/GradleImportExportPlugin.groovy
@@ -37,7 +37,8 @@ class GradleImportExportPlugin implements Plugin<Project> {
         }
         project.task('dataDelete') {
             group 'Data'
-            dependsOn project.rootProject.tasks.portalProperties, project.tasks.tomcatDeploy
+            dependsOn project.rootProject.tasks.portalProperties
+            mustRunAfter project.tasks.tomcatDeploy
             doFirst {
                 if (project.tasks.dataDelete.actions.size() == 1) {
                     logger.lifecycle('No actions have been defined for this task in this project')
