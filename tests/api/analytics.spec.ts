38b4a6a54f287c992df8a277ea18130704237239
Michael Gillian <mgillian-contractor@scholastic.com>
2022-09-20 04:28:19 -0700

    feature/EventAnalytics removed commented code

--- tests/api/analytics.spec.ts
+++ tests/api/analytics.spec.ts
@@ -27,25 +27,3 @@ test("post invalid link", async ({ request }) => {
   );
   expect(response.status()).toEqual(400);
 });
-
-//test("search type people", async ({ request }) => {
-//  await loginViaApi(request, "admin", "admin", "Amy Administrator");
-//  const response = await request.get(
-//    `${config.url}api/v5-0/portal/search?q=steven&type=people`
-//  );
-//  expect(response.status()).toEqual(200);
-//  expect(await response.json()).toEqual({
-//    people: [
-//      {
-//        uid: ["student"],
-//        telephoneNumber: ["(555) 555-5555"],
-//        mail: ["steven.student@example.org"],
-//        displayName: ["Steven Student"],
-//        givenName: ["Steven"],
-//        "user.login.id": ["student"],
-//        sn: ["Student"],
-//        username: ["student"],
-//      },
-//    ],
-//  });
-//});
