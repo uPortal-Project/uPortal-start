3c810683281dcaf60a506bf2ccb3595a95f21b07
Michael Gillian <mgillian-contractor@scholastic.com>
2022-09-30 07:44:03 -0700

    feature/EventAnalytics added additional playwright tests

--- tests/api/analytics.spec.ts
+++ tests/api/analytics.spec.ts
@@ -13,6 +13,7 @@ test("post valid link", async ({ request }) => {
     }
   );
   expect(response.status()).toEqual(201);
+  expect(await response.json()).toEqual({});
 });
 
 test("post invalid link", async ({ request }) => {
@@ -26,4 +27,18 @@ test("post invalid link", async ({ request }) => {
     }
   );
   expect(response.status()).toEqual(400);
+  expect(await response.json()).toEqual({
+    message:"Specified eventType is not the correct length or has invalid characters."
+    });
+});
+
+test("get logging level", async({ request}) => {
+  await loginViaApi(request, "admin", "admin", "Amy Administrator");
+    const response = await request.get(
+         `${config.url}api/analytics/level`
+  );
+  expect(response.status()).toEqual(200);
+  expect(await response.json()).toEqual({
+    level:"AUTHENTICATED"
+    });
 });
